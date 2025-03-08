88216545caa7f5bedb7214234a34566759255a05
Christian Brendlin <ch@brendlinonline.de>
2025-03-08 10:30:54 +0100

    prr

--- src/full/main.zig
+++ src/full/main.zig
@@ -1,2 +1,6 @@
-pub fn main() !void {}
+const ver = @import("zeta_version");
+const std = @import("std");
+pub fn main() !void {
+    std.debug.print("Build Hash {s}", .{ver.getVersionInfo().build_meta});
+}
 //fixed
