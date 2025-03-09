762a80ff6c14281a4f9363bf492adb4fdc7224d0
Christian Brendlin <ch@brendlinonline.de>
2025-03-09 22:23:44 +0100

    chore: reset version to 0.0.0 for initial development

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 // Constants for project configuration
 const PROJECT_NAME = "zeta";
-const VERSION = "0.0.0-alpha.1+0f92f62";
+const VERSION = "0.0.0";
 
 const VersionParts = struct {
     core: []const u8,
